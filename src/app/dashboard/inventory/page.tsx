84d42aaa77c9fff2b7afe73c77068ba06f0af5fd
Yago Modena <yagobmodena1@gmail.com>
2025-10-15 19:53:02 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogTrigger is not

--- src/app/dashboard/inventory/page.tsx
+++ src/app/dashboard/inventory/page.tsx
@@ -44,6 +44,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 

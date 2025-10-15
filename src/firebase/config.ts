5f29ca22d2dd151eb6af874411a170bf6b9f8800
Yago Modena <yagobmodena1@gmail.com>
2025-10-15 20:06:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3337313766-fb9b7",
+  "appId": "1:1028838481659:web:918643d426f80c49361a5e",
+  "apiKey": "AIzaSyB0z_R6EmixpKLhfaINB8HMx5lJKJo7epE",
+  "authDomain": "studio-3337313766-fb9b7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1028838481659"
+};
